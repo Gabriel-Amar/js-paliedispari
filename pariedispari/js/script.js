15a8f715bf2a2eb128e64ec01629ea1804311fcf
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-04-03 12:40:08 +0200

    js: parte finale

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,9 +1,12 @@
+//scelta pari o dispari
 let scelta = prompt("Scegli tra pari o dispari");
 console.log(scelta)
 
+//scelta numero 
 let numeroScelto = parseInt(prompt("Scegli un numero compreso tra 1 e 5"));
 console.log(numeroScelto);
 
+//funzione che genera un numero random
 function numeroRandom(min, max){
     let numero = Math.floor((Math.random() * max) + min)
     return numero;
@@ -12,13 +15,25 @@ function numeroRandom(min, max){
 let numeroGenerato = numeroRandom(1, 5);
 console.log(numeroGenerato);
 
+//somma a schermo dei due numeri
+console.log(numeroGenerato + numeroScelto)
+
+//funzione per definire se la somma è pari o dispari
 function sommaParidispari(num1, num2){
     let sommaNumeri = num1 + num2
     if(sommaNumeri % 2 === 0){
+        pari = true
         console.log(`La somma è ${sommaNumeri} ed è pari`)
     }else{
+        pari = false
         console.log(`La somma è ${sommaNumeri} ed è dispari`)
     }
 }
 
 let definizione = sommaParidispari(numeroGenerato, numeroScelto);
+
+if(pari === true && scelta === "pari"){
+    document.writeln("L'utente ha vinto")
+}else{
+    document.writeln("Il computer ha vinto")
+}
